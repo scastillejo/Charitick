514df735a726b55c9feccac5fbe9f1a1385c5be4
Sebastian <it.buenosaires@hotmail.com>
2017-02-22 09:28:04 -0300

    Redeploy

--- public/javascripts/aj-index.js
+++ public/javascripts/aj-index.js
@@ -23,33 +23,7 @@ $(document).ready(function(){
     url: '/',
     data: data,
     success: function(data){
-      let str = JSON.stringify(data);
-      if(str.substring(1,6) != 'Error'){
-        let leng = Object.keys(data).length;
-        if(leng > 0){
-          $("#searchresults").empty();
-          $('#searchresults').append('<p>Organization found:</p>');
-          $('#searchresults').append('<li id="fname">' + data.name + '</li>');
-          $('#searchresults').append('<p></p>');
-          $('#searchresults').append('<li id="fbriefdesc">' + data.briefdesc + '</li>');
-          $('#searchresults').append('<p></p>');
-          $('#searchresults').append('<li id="faddress">Address: ' + data.address + '</li>');
-          $('#searchresults').append('<li id="fstate">State: ' + data.state + '</li>');
-          $('#searchresults').append('<li id="fcity">City: ' + data.city + '</li>');
-          $('#searchresults').append('<li id="fzone">Zone/Locality: ' + data.zone + '</li>');
-          $('#searchresults').append('<li id="fphone">Phone: ' + data.phone + '</li>');
-          $('#searchresults').append('<li id="femail">Email: ' + data.email + '</li>');
-          $('#searchresults').append('<li id="fwebsite">Website: ' + data.website + '</li>');
-          $('#searchresults').append('<li id="fcategory">Category: ' + data.category + '</li>');
-          $('#searchresults').append('<li id="ftype">Type: ' + data.type + '</li>');
-          $('#searchresults').append('<p></p>');
-          $('#searchresults').append('<li id="ftime">Their time: ' + formatTime(data.hour) + ':' + formatTime(data.minute) + ':' + formatTime(data.second) + '</li>');
-          $('#searchresults').append('<p>Your time: ' + formatTime(hour) + ':' + formatTime(minute) + ':' + formatTime(second) + '</p>');
-          $("#resultform").show();
-        }
-      }            
-      else
-        alert(data);
+      onSuccessSearchOrg(data, hour, minute, second);
     },
     error:function(msg){
       alert(msg);
@@ -57,6 +31,36 @@ $(document).ready(function(){
   });
   return false;
 });
+
+let onSuccessSearchOrg = (data, hour, minute, second) => {
+  let str = JSON.stringify(data);
+  if(str.substring(1,6) != 'Error'){
+    let leng = Object.keys(data).length;
+    if(leng > 0){
+      $("#searchresults").empty();
+      $('#searchresults').append('<p>Organization found:</p>');
+      $('#searchresults').append('<li id="fname">' + data.name + '</li>');
+      $('#searchresults').append('<p></p>');
+      $('#searchresults').append('<li id="fbriefdesc">' + data.briefdesc + '</li>');
+      $('#searchresults').append('<p></p>');
+      $('#searchresults').append('<li id="faddress">Address: ' + data.address + '</li>');
+      $('#searchresults').append('<li id="fstate">State: ' + data.state + '</li>');
+      $('#searchresults').append('<li id="fcity">City: ' + data.city + '</li>');
+      $('#searchresults').append('<li id="fzone">Zone/Locality: ' + data.zone + '</li>');
+      $('#searchresults').append('<li id="fphone">Phone: ' + data.phone + '</li>');
+      $('#searchresults').append('<li id="femail">Email: ' + data.email + '</li>');
+      $('#searchresults').append('<li id="fwebsite">Website: ' + data.website + '</li>');
+      $('#searchresults').append('<li id="fcategory">Category: ' + data.category + '</li>');
+      $('#searchresults').append('<li id="ftype">Type: ' + data.type + '</li>');
+      $('#searchresults').append('<p></p>');
+      $('#searchresults').append('<li id="ftime">Their time: ' + formatTime(data.hour) + ':' + formatTime(data.minute) + ':' + formatTime(data.second) + '</li>');
+      $('#searchresults').append('<p>Your time: ' + formatTime(hour) + ':' + formatTime(minute) + ':' + formatTime(second) + '</p>');
+      $("#resultform").show();
+    }
+  }            
+  else
+    alert(data);
+} 
 
 let formatTime = (val) => {
     if(val < 10)
--- public/javascripts/aj-signup.js
+++ public/javascripts/aj-signup.js
@@ -4,66 +4,73 @@ $(document).ready(function(){
 
   $('form').on('submit', function(){
 
-      let data = {
-        username: $('#username').val(), 
-        password: $('#password').val(),
-        password2: $('#password2').val(),
-        hint: $('#hint').val()
-      };
+    let data = {
+      username: $('#username').val(), 
+      password: $('#password').val(),
+      password2: $('#password2').val(),
+      hint: $('#hint').val()
+    };
 
-      $.ajax({
-        async:true,
-        type: 'POST',
-        url: '/signup',
-        data: data,
-        success: function(data){
-          if(data != undefined){
-            let str = JSON.stringify(data);
-            if(str.substring(1,6) != 'Error'){
-              if($('#inf').text() == '-')
-                $('#btnsubmit').prop('disabled', true);
-            }
-            alert(data);
-          }
-        },
-        error:function(msg){
-          alert(msg);
-        }
-      });
-
-      return false;
+    $.ajax({
+      async:true,
+      type: 'POST',
+      url: '/signup',
+      data: data,
+      success: function(data){
+        onSuccessSignup(data);
+      },
+      error:function(msg){
+        alert(msg);
+      }
     });
 
-    $('#delaccount').on('click', function(){
-       let inf = $('#inf').text();
-       if(inf != undefined && inf != '' && inf != '-'){
-        if (confirm("Do you really want to delete your account?") == true) {
+    return false;
+  });
 
-          let deldata = {
-            inf: inf
-          };
+  let onSuccessSignup = (data) => {
+    if(data != undefined){
+      let str = JSON.stringify(data);
+      if(str.substring(1,6) != 'Error'){
+        if($('#inf').text() == '-')
+          $('#btnsubmit').prop('disabled', true);
+      }
+      alert(data);
+    }
+  };
 
-          $.ajax({
-            type: 'DELETE',
-            url: '/signup',
-            data: deldata,
-            success: function(msg){
-              if(msg == 'Account deleted.'){
-                $('#inf').text('-');
-                $('#btnsubmit').prop('disabled', true);
-                $('#delaccount').prop('disabled', true);
-              }
-              alert(msg);
-            },
-            error:function(msg){
-              alert(msg);
-            }          
-          });
-        }
-       }
-       
-       return false;
-    });
+  $('#delaccount').on('click', function(){
+     let inf = $('#inf').text();
+     if(inf != undefined && inf != '' && inf != '-'){
+      if (confirm("Do you really want to delete your account?") == true) {
+
+        let deldata = {
+          inf: inf
+        };
+
+        $.ajax({
+          type: 'DELETE',
+          url: '/signup',
+          data: deldata,
+          success: function(msg){
+            onSuccessDelete(msg);
+          },
+          error:function(msg){
+            alert(msg);
+          }          
+        });
+      }
+     }
+     return false;
+  });
+
+  let onSuccessDelete = (msg) => {
+    if(msg == 'Account deleted.'){
+      $('#inf').text('-');
+      $('#btnsubmit').prop('disabled', true);
+      $('#delaccount').prop('disabled', true);
+    }
+    alert(msg);
+  };
 });
 
 $(document).ajaxStart(function(){
